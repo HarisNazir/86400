4580c88a00c070aa5d479668896d562ffe64ae1d
Haris Nazir <haris.nazir1963@gmail.com>
2020-08-31 18:37:44 +0100

    Time Integration

---
+++ countdown.js
@@ -0,0 +1,7 @@
+var d = new Date()
+
+var currentHour = d.getHours()
+var currentMin = d.getMinutes()
+var currentSec = d.getSeconds()
+
+document.write( currentHour + ":" + currentMin + ":" + currentSec)
